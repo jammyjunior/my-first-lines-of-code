89d10b3a07d2a16f46e31fb0681755b6c64e3f2f
Jammy Jellyfish <trannguyenminhkhang0000@gmail.com>
2025-09-06 22:31:39 +0700

    Fix UI and Grammar

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -9,7 +9,7 @@ print("Type 'r' for rock, 'p' for paper, 's' for scissors or 'exit' to exit the 
 userChoice = input("Your choice: ").lower()
 
 # Check if the program cann't be run. userChoice not in acceptAnswer 
-userAnswerAccepted = ["rock", "r", "paper", "p", "s", "scissors" "exit"]
+userAnswerAccepted = ["rock", "r", "paper", "p", "s", "scissors", "exit"]
 while userChoice not in userAnswerAccepted:
     print("Sorry, I can't get it. Try again!")
     print("Type 'r' for rock, 'p' for paper, 's' for scissors or 'exit' to exit the program")
@@ -37,18 +37,22 @@ resultLoss = (userChoice == "rock" and systemChoice == "paper") or (userChoice =
 resultTie = userChoice == systemChoice
 
 # Print the result
+print()
 if resultWin == True:
-    print("System choice:", systemChoice)
+    print("Your chose:", userChoice)
+    print("System chose:", systemChoice)
     print()
     print("You won!")
 
 elif resultLoss == True:
-    print("System choice:", systemChoice)
+    print("Your chose:", userChoice)
+    print("System chose:", systemChoice)
     print()
     print("You lose.")
 
 elif resultTie == True:
-    print("System choice:", systemChoice)
+    print("Your chose:", userChoice)
+    print("System chose:", systemChoice)
     print()
     print("It's a tie!")
 
